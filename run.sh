5fbea5e165e88cdf140a43d1f2cdc3fcf3df2234
haivutuan93 <haivutuan93@gmail.com>
2024-06-07 22:54:45 +0700

    add file run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# fa-devops" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M develop
+git remote add origin https://github.com/haivutuan93/fa-devops.git
+git push -u origin develop
